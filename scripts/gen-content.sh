f73987789525bbb811a7052824e8e23074f70fd7
YiniXu9506 <xuyini@pingcap.com>
2019-05-27 18:03:21 +0800

    update gen-content.sh

--- scripts/gen-content.sh
+++ scripts/gen-content.sh
@@ -1,4 +1,3 @@
 node scripts/gen-nav.js 'content/docs/TOC.md' 'data/docs_toc.json' '/docs/'
 node scripts/gen-nav.js 'content/docs-cn/TOC.md' 'data/docs_cn_toc.json' '/docs-cn/'
 node scripts/gen-nav.js 'content/about-cn/recruit/TOC.md' 'data/recruit_cn_toc.json' '/about-cn/recruit/'
-node scripts/gen-nav.js 'content/blog-cn/TOC-User-Case.md' 'data/cases_cn_toc.json' '/cases-cn/'
